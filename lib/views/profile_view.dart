03f5016c95c11b989880078e52200c6ae9934075
Khaled Mostafa <95879454+El3aQeeD@users.noreply.github.com>
2023-02-10 03:00:55 +0200

    added bottom app bar edits in sign up

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -6,7 +6,7 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+      color: Colors.green,
     );
 
   }
